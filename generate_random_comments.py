fad56c96507b8002cf82691f739b687d6092f25f
andrea-w-wang <ayw255@nyu.edu>
2023-02-16 17:46:48 -0500

    first commit

---
+++ generate_random_comments.py
@@ -0,0 +1,27 @@
+import pickle as pk
+
+data_dir = "./data/"
+output_dir = "./data/samples/"
+
+def run(subreddit, nsamples):
+    usecols = ['year-month', 'text']
+    comments_df = pk.load(open(data_dir + f"{subreddit}-comments.pk", "rb"))
+    comments_df = comments_df[usecols]
+    comments_df['year'] = comments_df['year-month'].apply(lambda ym: int(ym.split('-')[0]))
+    comments_df = comments_df[comments_df['year'] >= 2014]
+    random_samples = comments_df.groupby("year-month").sample(n=nsamples, replace=False)[
+        ['year-month', 'text']].reset_index()
+    return random_samples
+
+
+if __name__ == '__main__':
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-sr", "--subreddit", required=True, type=str)
+    parser.add_argument("-n", "--nsamples", default=300, type=int)
+    parser.add_argument("-o", "--output", required=True, type=str)
+    args = parser.parse_args()
+
+    random_samples = run(args.subreddit, args.nsamples)
+    pk.dump(random_samples, open(f"{output_dir}{args.subreddit}/{args.output}.pk", "wb"))
