c88a6830ea5cb5ecc17b83a5c465fed2819d4f64
andrea-w-wang <ayw255@nyu.edu>
2023-02-16 18:44:26 -0500

    taking only 1 model month

--- calc_perplexity.py
+++ calc_perplexity.py
@@ -16,7 +16,7 @@ data_dir = "./data/samples"
 device_name = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-def run(subreddit, sample_filename, start_model_month, end_model_month, model_name="distilgpt2"):
+def run(subreddit, sample_filename, model_month, model_name="distilgpt2"):
     """
 
     :return: [{"model_month", "predict_month", "sample_file", "output"}]
@@ -27,23 +27,21 @@ def run(subreddit, sample_filename, start_model_month, end_model_month, model_na
     for predict_month in random_samples['year-month'].unique():
         selected_random_utts = list(random_samples[random_samples['year-month'] == predict_month]['text'].values)
 
-        model_month = start_model_month
-        while parse(model_month) <= parse(end_model_month):
-            stash = dict()
-            stash['predict_month'] = predict_month
-            stash['sample_filename'] = sample_filename
-            print(model_month)
-            checkpoint_path = f"./models/{model_name}_{subreddit}_{model_month}/best"
-            stash['model_month'] = model_month
-            stash['ppls'] = myppl._compute(
-                data=selected_random_utts,
-                model_id=checkpoint_path,
-                add_start_token=False,  # default,
-                max_length=1024
-            )
+        stash = dict()
+        stash['model_month'] = model_month
+        stash['predict_month'] = predict_month
+        stash['sample_filename'] = sample_filename
+        print(model_month)
+        checkpoint_path = f"./models/{model_name}_{subreddit}_{model_month}/best"
+        stash.update(myppl._compute(
+            data=selected_random_utts,
+            model_id=checkpoint_path,
+            add_start_token=False,  # default,
+            max_length=1024
+        ))
 
-            model_month = (parse(model_month) + relativedelta(months=1)).strftime("%Y-%m")
-            ppl_results.append(stash)
+        ppl_results.append(stash)
+    return ppl_results
 
 #
 # if __name__ == '__main__':
