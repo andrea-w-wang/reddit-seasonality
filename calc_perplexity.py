bebfd24fb59cbd001f100046c45ee774e7ec8f99
andrea-w-wang <ayw255@nyu.edu>
2023-02-16 18:23:43 -0500

    rewrite

--- calc_perplexity.py
+++ calc_perplexity.py
@@ -10,51 +10,47 @@ from scipy import stats
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from transformers import Trainer, TrainingArguments
 
-data_dir = "./data/"
+import metrics
+
+data_dir = "./data/samples"
 device_name = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-# https://huggingface.co/spaces/evaluate-metric/perplexity
+def run(subreddit, sample_filename, start_model_month, end_model_month, model_name="distilgpt2"):
+    """
 
-def run(subreddit, model_month, model_name="distilgpt2"):
-    usecols = ['year-month', 'timestamp', 'text', 'speaker']
-    comments_df = pk.load(open(data_dir + f"{subreddit}-comments.pk", "rb"))
-    comments_df = comments_df[usecols]
+    :return: [{"model_month", "predict_month", "sample_file", "output"}]
+    """
+    random_samples = pk.load(open(f"{data_dir}/{subreddit}-{sample_filename}.pk", "rb"))
+    ppl_results = list()
+    myppl = metrics.myPerplexity()
+    for predict_month in random_samples['year-month'].unique():
+        selected_random_utts = list(random_samples[random_samples['year-month'] == predict_month]['text'].values)
 
-    perplexity = evaluate.load("perplexity", module_type="metric")
-    checkpoint_name = f"{model_name}_{subreddit}_{model_month}"
-    checkpoint = f"./models/{checkpoint_name}/best"
-    print(checkpoint_name)
-    max_length = 512
+        model_month = start_model_month
+        while parse(model_month) <= parse(end_model_month):
+            stash = dict()
+            stash['predict_month'] = predict_month
+            stash['sample_filename'] = sample_filename
+            print(model_month)
+            checkpoint_path = f"./models/{model_name}_{subreddit}_{model_month}/best"
+            stash['model_month'] = model_month
+            stash['ppls'] = myppl._compute(
+                data=selected_random_utts,
+                model_id=checkpoint_path,
+                add_start_token=False,  # default,
+                max_length=1024
+            )
 
-    last_month = comments_df['year-month'].max()
-    ppl_results = dict()
-    predict_month = model_month
-    while predict_month <= last_month:
-        print(f"***Predict {predict_month}***")
+            model_month = (parse(model_month) + relativedelta(months=1)).strftime("%Y-%m")
+            ppl_results.append(stash)
 
-        input_texts = [t[:max_length] for t in
-                       comments_df[comments_df['year-month'] == predict_month]['text']
-                       if t != ""]
-        if len(input_texts) == 0:
-            print(f"{predict_month}: no data")
-            continue
-        ppl_results[predict_month] = perplexity.compute(
-            model_id=checkpoint,
-            add_start_token=True,  # default
-            predictions=input_texts
-        )
-        predict_month = (parse(predict_month) + relativedelta(months=1)).strftime("%Y-%m")
-
-    pk.dump(ppl_results,
-            open(data_dir + f"output/{checkpoint_name}_scores.pk", "wb"))
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-sr", "--subreddit", required=True, type=str)
-    parser.add_argument("-month", "--model-month", required=True, type=str)
-    parser.add_argument("-m", "--model-name", default='distilgpt2', type=str)
-    args = parser.parse_args()
-
-    run(args.subreddit, args.model_month, args.model_name)
+#
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("-sr", "--subreddit", required=True, type=str)
+#     parser.add_argument("-month", "--model-month", required=True, type=str)
+#     parser.add_argument("-m", "--model-name", default='distilgpt2', type=str)
+#     args = parser.parse_args()
+#
+#     run(args.subreddit, args.model_month, args.model_name)
