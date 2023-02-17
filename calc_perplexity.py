4f42f8a01731cfdaadfc80514beef9cfdfa85034
andrea-w-wang <ayw255@nyu.edu>
2023-02-16 21:14:01 -0500

    change function to 1-1

--- calc_perplexity.py
+++ calc_perplexity.py
@@ -1,60 +1,64 @@
 import argparse
+import evaluate
+import numpy as np
 import pickle as pk
 import torch
-from datasets import load_dataset, Dataset
 from dateutil.parser import parse
 from dateutil.relativedelta import relativedelta
-from scipy import stats
-import datasets
-import numpy as np
-import torch
-from torch.nn import CrossEntropyLoss
-from transformers import AutoModelForCausalLM, AutoTokenizer
 
-import evaluate
-from evaluate import logging
+import metrics
 
+data_dir = "./data/"
 device = "cuda" if torch.cuda.is_available() else "cpu"
-data_dir = "./data/samples"
+
+np.random.seed(0)
 
 
-def run(subreddit, sample_filename, model_month, model_name="distilgpt2"):
-    """
+def calculate_my_ppl(checkpoint_path, sentences):
+    perplexity = metrics.myPerplexity()
+    return perplexity.compute(
+        model_id=checkpoint_path,
+        add_start_token=False,  # default
+        data=sentences,
+        max_length=1024
+    )
 
-    :return: [{"model_month", "predict_month", "sample_file", "output"}]
-    """
-    random_samples = pk.load(open(f"{data_dir}/{subreddit}-{sample_filename}.pk", "rb"))
-    ppl_results = list()
-    myppl = myPerplexity()
-    for predict_month in random_samples['year-month'].unique():
-        selected_random_utts = list(random_samples[random_samples['year-month'] == predict_month]['text'].values)
 
-        stash = dict()
-        stash['model_month'] = model_month
-        stash['predict_month'] = predict_month
-        stash['sample_filename'] = sample_filename
-        print(model_month)
-        checkpoint_path = f"./models/{model_name}_{subreddit}_{model_month}/best"
-        stash.update(myppl._compute(
-            data=selected_random_utts,
-            model_id=checkpoint_path,
-            add_start_token=False,  # default,
-            max_length=1024
-        ))
+def run(subreddit, predict_month, model_month, model_name="distilgpt2"):
+    usecols = ['year-month', 'text']
+    comments_df = pk.load(open(data_dir + f"{subreddit}-comments.pk", "rb"))
+    comments_df = comments_df[usecols]
 
-        ppl_results.append(stash)
+    input_texts = [t for t in comments_df[comments_df['year-month'] == predict_month]['text'] if t]
+    selected_random_utts = list(np.random.choice(input_texts, size=300, replace=False))
+
+    ppl_results = {"predict_month": predict_month,
+                   "utterances": selected_random_utts,
+                   "model_month": model_month
+                   }
+
+    checkpoint_path = f"./models/{model_name}_{subreddit}_{model_month}/best"
+    my_ppl = calculate_my_ppl(checkpoint_path, selected_random_utts)
+    ppl_results.update(my_ppl)
+
     return ppl_results
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-sr", "--subreddit", required=True, type=str)
-    parser.add_argument("-f", "--sample-filename", required=True, type=str)
-    parser.add_argument("-month", "--model-month", required=True, type=str)
-    parser.add_argument("-m", "--model-name", default='distilgpt2', type=str)
+    parser.add_argument("-r", "--subreddit", required=True, type=str)
+    parser.add_argument("-pm", "--predict-month", required=True, type=str)
+    parser.add_argument("-smm", "--start-model-month", required=True, type=str)
+    parser.add_argument("-emm", "--end-model-month", required=True, type=str)
     args = parser.parse_args()
-    output = run(args.subreddit, args.sample_filename, args.model_month, args.model_name)
-    pk.dump(output, open(
-        f"./data/output/{args.subreddit}-{args.sample_filename}-model_month{args.model_month}.pk", "wb"
-    ))
 
+    model_month = args.start_model_month
+
+    while parse(model_month) <= parse(args.end_model_month):
+        print(model_month)
+        output = run(args.subreddit, args.predict_month, model_month)
+        pk.dump(output, open(
+            f"./data/output/{args.subreddit}-model{model_month}-predict{args.predict_month}.pk", "wb"
+        ))
+
+        model_month = (parse(model_month) + relativedelta(months=1)).strftime("%Y-%m")
