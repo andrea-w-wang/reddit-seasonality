8fb32c7e14b620aadc7ba6a6c40f56557046ac26
andrea-w-wang <ayw255@nyu.edu>
2023-02-17 17:39:14 -0500

    first commit

---
+++ embed_sentence.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import pickle as pk
+import seaborn as sns
+# initialize ST5 model
+from sentence_transformers import SentenceTransformer
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+
+def run(subreddit):
+    model = SentenceTransformer('sentence-transformers/sentence-t5-base')
+
+    df = pk.load(open(f"data/{subreddit}-comments.pk", "rb"))
+    sentences = list(df['text'].values)
+
+    embeddings = model.encode(sentences)
+    return embeddings
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-sr", "--subreddit", required=True, type=str)
+    args = parser.parse_args()
+    embeddings = run(args.subreddit)
+    pk.dump(random_samples, open(f"./data/{args.subreddit}-sentence-embeddings.pk", "wb"))
