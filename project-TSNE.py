b01840b4ec43b6c4049ebc9878a0807ce090dc73
andrea-w-wang <ayw255@nyu.edu>
2023-02-21 16:36:53 -0500

    first commit

---
+++ project-TSNE.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+import pandas as pd
+import numpy as np
+import pickle as pk
+import seaborn as sns
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--perplexity", required=True, type=float)
+args = parser.parse_args()
+
+combined_emb_df = pk.load(open("./data/combined_emb_df.pk", "rb"))
+embeddings = combined_emb_df.drop(['id', 'year-month', 'file'], axis=1).values
+method = TSNE(n_components=2, perplexity=args.perplexity)
+projection = method.fit_transform(embeddings)
+metadata = combined_emb_df[['id', 'year-month', 'file']].reset_index(drop=True)
+metadata['subreddit'] = metadata['file'].apply(lambda f: f.split("-")[0])
+xdf = pd.concat((pd.DataFrame(projection), metadata), axis=1)
+sns.scatterplot(data=xdf, x=0, y=1, hue='subreddit')
+plt.title(f"TSNE-perplexity_{args.perplexity}")
+plt.savefig(f"./figures/TSNE-perplexity_{args.perplexity}.png")
+
+
---
+++ project-UMAP.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pickle as pk
+import seaborn as sns
+import umap
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-n", "--n_neighbors", required=True, type=float)
+parser.add_argument("-d", "--min_dist", required=True, type=float)
+args = parser.parse_args()
+
+combined_emb_df = pk.load(open("./data/combined_emb_df.pk", "rb"))
+embeddings = combined_emb_df.drop(['id', 'year-month', 'file'], axis=1).values
+
+method = umap.UMAP(
+        n_neighbors=args.n_neighbors,
+        min_dist=args.min_dist,
+        n_components=2,
+        metric='euclidean'
+    )
+projection = method.fit_transform(embeddings)
+metadata = combined_emb_df[['id', 'year-month', 'file']].reset_index(drop=True)
+metadata['subreddit'] = metadata['file'].apply(lambda f: f.split("-")[0])
+xdf = pd.concat((pd.DataFrame(projection), metadata), axis=1)
+sns.scatterplot(data=xdf, x=0, y=1, hue='subreddit')
+plt.title(f"UMAP-n_neighbors={args.n_neighbors}-min_dist={min_dist}")
+plt.savefig(f"./figures/UMAP-neighbors_{args.n_neighbors}-mindist_{args.min_dist}.png")
