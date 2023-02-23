c166911f8824ad928df5a36ccfb0fb9aa51febe1
andrea-w-wang <ayw255@nyu.edu>
2023-02-23 12:52:18 -0500

    first commit

---
+++ jsd.py
@@ -0,0 +1,47 @@
+import collections.abc
+import pickle as pk
+from collections import Counter
+
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.util import ngrams
+
+collections.Mapping = collections.abc.Mapping
+
+data_dir = "./data/"
+
+
+def count_ngrams(input_texts, n=1):
+    ngrams_counter = Counter()
+    for text in input_texts:
+        text_ngrams = ngrams(word_tokenize(text), n)
+        ngrams_counter.update(text_ngrams)
+    return ngrams_counter
+
+
+def get_ngrams_counter(utt_fp):
+    ngrams_counter = dict()
+
+    data = pk.load(open(utt_fp, "rb"))
+    df = pd.DataFrame(list(data))
+    months = df['year-month'].unique()
+    for m in months:
+        print("\t", m)
+        monthly_comments = df[df['year-month'] == m]['text'].tolist()
+        ngrams_counter[m] = count_ngrams(monthly_comments)
+
+    return ngrams_counter
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--subreddit", required=True, type=str)
+    args = parser.parse_args()
+
+    print(args.subreddit)
+
+    sample_df_folder = './data/samples/'
+    utt_fp = f"{sample_df_folder}{args.subreddit}-comments.pk"
+    stash = get_ngrams_counter(utt_fp)
