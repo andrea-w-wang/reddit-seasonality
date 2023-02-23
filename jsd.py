8832a3ba827143baa421dc6acd4b13a1e388f45e
andrea-w-wang <ayw255@nyu.edu>
2023-02-23 13:29:19 -0500

    add rank calculations and convert to long form

--- jsd.py
+++ jsd.py
@@ -1,7 +1,11 @@
 import argparse
+import pickle as pk
 from collections import defaultdict
 from itertools import combinations
-import pickle as pk
+
+import numpy as np
+import pandas as pd
+
 import metrics
 
 parser = argparse.ArgumentParser()
@@ -24,3 +28,22 @@ for month_1, month_2 in month_pairs:
                          base=2)
 
     jsd_diffs[month_1][month_2] = my_jsd.total_diff
+
+first_month = all_months[0]
+last_month = all_months[-1]
+jsd_diffs[first_month][first_month] = np.nan
+jsd_diffs[last_month][last_month] = np.nan
+
+jsd_df = pd.DataFrame(jsd_diffs)
+jsd_df = jsd_df.sort_index()
+X = jsd_df.fillna(0).values
+X2 = X + X.T - np.diag(np.diag(X))
+jsd_df = pd.DataFrame(X2, columns=jsd_df.columns, index=jsd_df.index)
+jsd_df = jsd_df.replace(0, np.nan)
+
+long = jsd_df.unstack().reset_index()
+long = long.sort_values(["level_0", "level_1"])
+long = long.rename({"level_0": "month_1", "level_1": "month_2", 0: "jsd"}, axis=1)
+long['jsd_rank'] = long.groupby("month_1")["jsd"].rank()
+
+pk.dump(long, open(f"data/output/long/jsd/{args.subreddit}.pk", "wb"))
