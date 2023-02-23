eea43e22c1baf9b59ac4fd1962bdbee42ed762fa
andrea-w-wang <ayw255@nyu.edu>
2023-02-23 16:41:04 -0500

    change to embed all

--- embed_sentence.py
+++ embed_sentence.py
@@ -11,17 +11,12 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--subreddit", required=True, type=str)
 args = parser.parse_args()
+
 print(args.subreddit)
 sample_df_folder = './data/samples/'
 data = pk.load(open(f"{sample_df_folder}{args.subreddit}-comments.pk", "rb"))
 model = SentenceTransformer('sentence-transformers/sentence-t5-base', device=device)
 
-for i in range(10):
-    print(f"sample {i+1}")
-    sample = np.random.choice(data, size=50000, replace=False)
-    sentences = [x['text'] for x in sample]
-    embeddings = model.encode(sentences)
-    monthly_embed = pd.DataFrame(embeddings).groupby([x['year-month'] for x in sample]).mean()
-    stash = (np.array(monthly_embed.index), monthly_embed.values)
-
-    pk.dump(stash, open(f"./data/output/embeddings/{args.subreddit}-sample{i+1}.pk", "wb"))
+sentences = [x['text'] for x in data]
+embeddings = model.encode(sentences)
+pk.dump(embeddings, open(f"./data/output/embeddings/{args.subreddit}.pk", "wb"))
