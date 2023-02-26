59bf7c005ff852a274b8b613186250953af09773
andrea-w-wang <ayw255@nyu.edu>
2023-02-26 01:10:59 -0500

    fix

--- jsd_regression.py
+++ jsd_regression.py
@@ -41,8 +41,7 @@ def run_one_sample(metadata):
     mod = OrderedModel.from_formula("jsd_rank ~ C(same_year) + C(same_month) + months_apart", data=long.dropna(),
                                     distr='logit')
 
-    mod = mod.fit(method='bfgs')
-    rank_res = mod.fit()
+    rank_res = mod.fit(method='bfgs')
 
     return dist_res, rank_res
 
